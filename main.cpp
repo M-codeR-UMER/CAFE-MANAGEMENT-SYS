1765557e65a386c4c235359ed8cc946a23fa4e73
M-codeR-UMER <155852130+M-codeR-UMER@users.noreply.github.com>
2024-06-18 16:38:54 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,11 +19,25 @@ void Enter_to_exit() // This is a Function by which pressing ENTER it stops ....
     cin.get();
     exit(0);
 }
+void Enter_to_continue() // This is a Function by which pressing ENTER it responds....
+{
+    cout << endl;
+    cout << setw(20) << " " << "Press ENTER to CONTINUE...";
+    cin.get();
+    // exit(0);
+}
 class POS
 {
     // Revenue :
     // payment processing
     // printing recipts on screen
+
+public:
+    int quantity = 0;
+    double price = 0;
+    double total_price()
+    {
+    }
 };
 class Owner
 {
@@ -87,15 +101,15 @@ public:
                 }
                 else
                 {
-                    cout << "Incorrect password.";
-                    return false;
+                    system("cls");
+                    cout << setw(5) << " " <<"Incorrect password.";
                     exit(0);
                 }
             }
             else
             {
-                cout << "Incorrect username.";
-                return false;
+                system("cls");
+                cout << setw(5) << " " << "Incorrect username.";
                 exit(0);
             }
         }
@@ -163,7 +177,8 @@ class Menu : public POS // Inheriting POS in Menu.
 public:
     string items_names[6] = {"TEA", "COFFEE", "BLACK COFFEE", "CAKE", "SANDWICH", "WAFFLES"};
     float items_prices[6] = {10.0, 15.0, 12.0, 27.0, 17.0, 25.0};
-    Menu()
+    Menu() {}
+    void displayMenu()
     {
         system("cls");
         cout << setw(20) << "" << "--------------------------------------" << endl;
@@ -171,9 +186,66 @@ public:
         cout << setw(20) << "" << "--------------------------------------" << endl;
         for (int i = 0; i < 6; i++)
         {
-            cout << setw(20) << " " <<"|"<< i + 1 << ". " << items_names[i] <<setw(31-items_names[i].length())<<" "<<items_prices[i]<<"|"<<endl;
+            cout << setw(20) << " " << "|" << i + 1 << ". " << items_names[i] << setw(30 - items_names[i].length()) << " " << items_prices[i] << "$|" << endl;
         }
         cout << setw(20) << "" << "--------------------------------------" << endl;
+        cout << endl;
+    }
+    void continuity_function()
+    {
+        char choice;
+        cout << "Do you want to continue? (Y/N): ";
+        cin >> choice;
+        cout<<endl;
+        if (choice == 'N' || choice == 'n') // Exit the program if user chooses to not continue.
+        {
+            cout << "Your Total Price is : " << price << "$" << endl;
+            exit(0);
+        }
+    }
+    void price_calculator()
+    {
+        int ch;
+        while (true)
+        {
+            cout << "Enter Your Choice : ";
+            cin >> ch;
+            if (ch < 0 && ch > 6)
+            {
+                cout << "Invaild Choice." << endl;
+                break;
+            }
+            switch (ch)
+            {
+            case 1:
+                price = price + items_prices[0];
+                continuity_function();
+                break;
+            case 2:
+                price = price + items_prices[1];
+                continuity_function();
+                break;
+            case 3:
+                price = price + items_prices[2];
+                continuity_function();
+                break;
+            case 4:
+                price = price + items_prices[3];
+                continuity_function();
+                break;
+            case 5:
+                price = price + items_prices[4];
+                continuity_function();
+                break;
+            case 6:
+                price = price + items_prices[5];
+                continuity_function();
+                break;
+            default:
+                cout << "Your Total Price is : " << price << "$" << endl;
+                Enter_to_exit() ;
+            }
+        }
     }
 };
 int ownercode;
@@ -184,10 +256,15 @@ int main()
     cout << setw(20) << "" << "--------------------------------------" << endl;
 
     cout << endl;
+    while(true){
+    Owner o;
+    o.set_data();
+    o.check_credentails();
+    o.Welcoming_owner();
+    Enter_to_continue();
 
-    // Owner o;
-    // o.set_data();
-    // o.check_credentails();
-    // o.Welcoming_owner();
-    Menu();
+    Menu M;
+    M.displayMenu();
+    M.price_calculator();
+    }
 }
